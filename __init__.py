60877898700b0c424dec85ea97df4ce8317dada1
Mert <62549656+fswair@users.noreply.github.com>
2022-10-21 19:56:09 +0300

    olur öyle ufak hatalar

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 Name: TomrisUserBot
 Repo: https://github.com/fswair/TomrisUserBot
 Version: 1.0
-Licence: GNU General Public Licence v3.0
+License: GNU General Public License v3.0
 Author(s):
     @fswair
     @ixelizm
